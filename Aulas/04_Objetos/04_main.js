8973a23b04900d5c590a2260dd096c1a66ee528e
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2026-01-23 21:56:07 -0300

    update: inserindo um novo exemplo e um desafio

--- Aulas/04_Objetos/04_main.js
+++ Aulas/04_Objetos/04_main.js
@@ -12,14 +12,47 @@ const person = {
   ],
 };
 
-const firstName = person.firstName;
-const lastName = person.lastName;
-const age = person.age;
-const email = person.email;
-const hobbies = person.hobbies[3];
+// const firstName = person.firstName;
+// const lastName = person.lastName;
+// const age = person.age;
+// const email = person.email;
+// const hobbies = person.hobbies[3];
+
+// Mesma coisa que acima, mas de uma forma mais limpa
+// Destructuring
+const { firstName, lastName, age, careca, email, hobbies } = person;
 
 console.log(firstName);
 console.log(lastName);
 console.log(age);
+console.log(careca);
 console.log(email);
-console.log(hobbies);
+console.log(hobbies[3]);
+
+// Adicionando elementos novos ao objeto
+person.dog = "Pastor Alemão";
+console.log(person.dog);
+
+// Outro Exemplo
+const toDos = [
+  {
+    id: 1,
+    description: "Estudar JavaScript",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    description: "Enviar todos os estudos para o Github",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    description: "Estudar Versionamento de Código",
+    isCompleted: true,
+  },
+];
+
+// Desafio: acessar a lista e imprimir no console a description do item 3
+const descriptionLastItemToDo = toDos[2].description;
+
+console.log(descriptionLastItemToDo);
